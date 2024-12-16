54acd094ddd17fff601f0ef373d9a9ca31a72b0e
Shanu Kumar <shanukumar@Shanus-MacBook-Air.local>
2024-12-17 01:58:13 +0530

    changes

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,34 +1,37 @@
 'use client'
 import MeetingSetUp from '@/components/MeetingSetUp';
 import MeetingRoom from '@/components/MeetingRoom';
-import { useUser } from '@clerk/nextjs'
+import { useUser } from '@clerk/nextjs';
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useGetCallById } from '@/hooks/useGetCallById';
 import Loader from '@/components/Loader';
 
-interface MeetingProps {
+// Type for dynamic route params
+interface MeetingPageProps {
   params: {
     id: string;
   };
 }
 
-const Meeting: React.FC<MeetingProps> = ({params}) => {
-  const {isLoaded} = useUser();
-  const [isSetUpComplete,setIsSetUpComplete] = useState(false);
-  const {call , isCallLoading} = useGetCallById(params?.id);
-   if(!isLoaded || isCallLoading) return <Loader />
+const Meeting: React.FC<MeetingPageProps> = ({ params }) => {
+  const { isLoaded } = useUser();
+  const [isSetUpComplete, setIsSetUpComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(params.id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
   return (
-    <main className='h-screen  w-full'>
-        <StreamCall call={call}>
-            <StreamTheme>
-               {
-                   !isSetUpComplete ? <MeetingSetUp setIsSetUpComplete= {setIsSetUpComplete}/> : <MeetingRoom />
-               }
-            </StreamTheme>
-        </StreamCall>
+    <main className='h-screen w-full'>
+      <StreamCall call={call}>
+        <StreamTheme>
+          {
+            !isSetUpComplete ? <MeetingSetUp setIsSetUpComplete={setIsSetUpComplete} /> : <MeetingRoom />
+          }
+        </StreamTheme>
+      </StreamCall>
     </main>
-  )
+  );
 }
 
-export default Meeting
+export default Meeting;
